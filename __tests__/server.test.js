d0df54ee067dbfeb3ac776230a0534986a69674a
Chris Gantt <gantt.art@gmail.com>
2021-04-21 17:16:40 -1000

    Created routes for family endpoints and created tests, all passing

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -5,19 +5,7 @@ const supertest = require('supertest');
 const { expect } = require('@jest/globals');
 const mockRequest = supertest(server);
 
-describe('Test Basic API Server', () => {
-
-  it('should respond with a 404 on a bad route', async () => {
-    return mockRequest.get('/badroute').then(data => {
-      expect(data.status).toBe(404);
-    });
-  });
-
-  it('should respond with a 404 on a bad method', async () => {
-    const response = await mockRequest.put('/pet');
-    expect(response.status).toBe(404);
-  });
-
+describe('Test API pet routes', () => {
   it('should create a new pet in the db', async () => {
     const response = await mockRequest.post('/pet').send({ name: 'Angel', species: 'dog' });
     expect(response.status).toBe(201);
@@ -58,5 +46,60 @@ describe('Test Basic API Server', () => {
     expect(response.body.length).toEqual(1);
     expect(response.body[0].id).toEqual(2);
   });
+});
 
+describe('Test API family routes', () => {
+  it('should create a new family member in the db', async () => {
+    const response = await mockRequest.post('/family').send({ name: 'test-spouse', relation: 'spouse' });
+    expect(response.status).toBe(201);
+    expect(response.body.record.name).toEqual('test-spouse');
+    expect(response.body.record.relation).toEqual('spouse');
+  });
+
+  it('should retrieve a pet from the db', async () => {
+    const response = await mockRequest.get('/family/1');
+    expect(response.status).toBe(200);
+    expect(response.body.record.name).toEqual('test-spouse');
+    expect(response.body.record.relation).toEqual('spouse');
+  });
+
+  it('should retrieve all pets from the db', async () => {
+    await mockRequest.post('/family').send({ name: 'test-father', relation: 'father' });
+    const response = await mockRequest.get('/family');
+    expect(response.status).toBe(200);
+    expect(response.body.length).toEqual(2);
+    expect(response.body[1].id).toEqual(2);
+    expect(response.body[1].record.name).toEqual('test-father');
+  });
+
+  it('should update a pet in the db', async () => {
+    const response = await mockRequest.put('/family/1').send({ name: 'test-spouse', relation: 'partner' });
+    const pet = await mockRequest.get('/family/1');
+    expect(response.status).toBe(200);
+    expect(pet.body.record.relation).toEqual('partner');
+  });
+
+  it('should not delete a pet if the id is not in the db', async() => {
+    const response = await mockRequest.delete('/family/100');
+    expect(response.text).toEqual('Family member with ID not in DB');
+  })
+
+  it('should delete a pet in the db', async () => {
+    const response = await mockRequest.delete('/family/1');
+    expect(response.body.length).toEqual(1);
+    expect(response.body[0].id).toEqual(2);
+  });
+});
+
+describe('Test API Error Handling', () => {
+  it('should respond with a 404 on a bad route', async () => {
+    return mockRequest.get('/badroute').then(data => {
+      expect(data.status).toBe(404);
+    });
+  });
+
+  it('should respond with a 404 on a bad method', async () => {
+    const response = await mockRequest.put('/pet');
+    expect(response.status).toBe(404);
+  });
 });
--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@
 const express = require('express');
 const app = express();
 
+const familyRoutes = require('./routes/family-routes.js');
 const petRoutes = require('./routes/pet-routes.js');
 const notFound = require('./error-handlers/404.js');
 
 app.use(express.json());
 
+app.use(familyRoutes);
 app.use(petRoutes);
 app.use('*', notFound);
 
